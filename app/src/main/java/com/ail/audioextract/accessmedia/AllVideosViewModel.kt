d419817cf9edc8e6192b64ed5f09032faeed60d1
akarsh <akarshpawar23396@gmail.com>
2020-06-18 13:12:46 +0530

    commit

--- app/src/main/java/com/ail/audioextract/accessmedia/AllVideosViewModel.kt
+++ app/src/main/java/com/ail/audioextract/accessmedia/AllVideosViewModel.kt
@@ -206,7 +206,7 @@ class BaseMainViewModel(application: Application) : AndroidViewModel(application
                     commonFile.file_path = cursor.getString(videoData)
                     commonFile.createdTime = dateModified
                     commonFile.lastPlayedDuration = cursor.getLong(bookmark)
-                    commonFile.duration = durations.toString()
+                    commonFile.duration = cursor.getLong(durations).toString()
                     commonFile.isDirectory = false
                     commonFile.setFindDuplicate(false)
                     if (commonFile.file_path != null && !commonFile.file_path.equals("")) {
